74ef84fe57d27dfbcedc58d6438ff14ca516c13a
uday <uday.shinde3188@gmail.com>
2025-06-27 23:18:34 +0530

    new ip cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const allowOrigins = [
     'http://localhost:4200',
-    'http://15.206.165.173',
+    'http://13.233.237.187',
     'https://dev-tinder.info',
     'https://www.dev-tinder.info',
     'https://api.dev-tinder.info'
